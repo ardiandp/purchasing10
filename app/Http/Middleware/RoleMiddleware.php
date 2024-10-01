2abe14b5000846760cc1757b1c8bba213027ae49
unknown <ardiandp>
2024-10-01 23:04:20 +0700

    mulai admin

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -13,8 +13,17 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-        return $next($request);
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        $user = Auth::user();
+        if (in_array($user->role->role_name, $roles)) {
+            return $next($request);
+        }
+
+        return abort(403, 'Unauthorized action.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StaffGAController;
+use App\Http\Controllers\HeadGAController;
+use App\Http\Controllers\DivisiController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route untuk Admin
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::resource('admin/divisi', DivisiController::class);
+    Route::resource('admin/barang', BarangController::class);
+    Route::resource('admin/users', UserController::class);
+    Route::resource('admin/suppliers', SupplierController::class);
+    // Tambahkan route admin lainnya
 });
